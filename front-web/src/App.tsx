6064ae8f983d18b01ab6a202534bebc8eed09d32
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-06 23:51:15 -0300

    add routes and stepsheader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css';
-import Home from './Home';
-import NavBar from './Navbar';
+import Routes from './Routes';
 
 function App() {
 
   return (
-    <>
-      <NavBar/>
-      <Home />
-    </>
+    <Routes />
   );
 }
 
