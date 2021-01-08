743ed53038036b99bf401742d8b2a140f6ab535f
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-08 15:38:54 -0300

    adding Order

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -10,3 +10,7 @@ export function fetchProducts(){
 export function fetchLocalMapBox(local: string){
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
 }
+
+export function saveOrder(){
+    
+}
