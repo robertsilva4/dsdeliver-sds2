29ea027f45d72c9c50c85b981aa01bca210042c4
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-09 21:32:11 -0300

    Add order list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+
+  const navigation = useNavigation();
+
+    const handleOnPress = () => {
+        navigation.navigate('Home');
+    }
+
   return (
-    <View style={styles.container}>
-        <Image source={require('../assets/logo.png')}/>
-      <Text style={styles.text}>DS Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require('../assets/logo.png')} />
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -15,10 +26,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#DA5C5C",
     height: 90,
     paddingTop: 50,
-    flexDirection:'row',
+    flexDirection: 'row',
     justifyContent: 'center'
   },
-  text:{
+  text: {
     fontWeight: 'bold',
     fontSize: 18,
     lineHeight: 25,
